2169aaf190f223cb73d5a8278a62bbca20ff1c5f
guy <luckythelich@hotmail.com>
2017-08-11 10:30:29 -0700

    viewresult for HomeController/index passed.

---
+++ Seafood.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCodSeafood.Controllers;
+using SonOfCodSeafood.Models;
+using Xunit;
+
+namespace Seafood.Tests.ControllerTests
+{
+    public class HomeControllerTest
+    {
+
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
