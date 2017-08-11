bdf1ac7f3fb9202a5467068883e2190a04e16890
guy <luckythelich@hotmail.com>
2017-08-11 15:38:34 -0700

    Create account controller and view.

---
+++ src/SonOfCodSeafood/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCodSeafood.Models;
+
+namespace SonOfCodSeafood.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly SonOfCodSeafoodDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, SonOfCodSeafoodDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
