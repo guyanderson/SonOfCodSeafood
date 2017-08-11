a96ce67454dfbdd5854d788449ebe1c67972c094
guy <luckythelich@hotmail.com>
2017-08-11 11:10:15 -0700

    Get Product<list> test passed.

--- Seafood.Tests/ModelTests/ProductTest.cs
+++ Seafood.Tests/ModelTests/ProductTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SonOfCodSeafood.Models;
+using SonOfCodSeafood.Controllers;
 using Xunit;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Seafood.Tests.ModelTests
 {
@@ -22,6 +24,20 @@ namespace Seafood.Tests.ModelTests
 
             //Assert
             Assert.Equal("Cod", result);
+        }
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            ViewResult indexView = new ProductController().Index() as ViewResult;
+
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Product>>(result);
         }
 
     }
--- src/SonOfCodSeafood/Controllers/ProductController.cs
+++ src/SonOfCodSeafood/Controllers/ProductController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SonOfCodSeafood.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SonOfCodSeafood.Controllers
 {
     public class ProductController : Controller
     {
- 
+
+        private SonOfCodSeafoodDbContext db = new SonOfCodSeafoodDbContext();
+
         public IActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
         }
+
     }
 }
