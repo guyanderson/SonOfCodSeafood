df03e365c5ca8531b0f364cfa5e9fb55f7d39a3b
guy <luckythelich@hotmail.com>
2017-08-11 10:23:25 -0700

    commit failing test

---
+++ Seafood.Tests/ControllerTests/ProductsControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCodSeafood.Controllers;
+using SonOfCodSeafood.Models;
+using Xunit;
+
+namespace Seafood.Tests.ControllerTests
+{
+    public class ProductsControllerTest
+    {
+
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ProductController controller = new ProductController();
+
+            //Act
+            var result = controller.details();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+    }
+}
